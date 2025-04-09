6d8e526e9a6d9542f9a00a25e8b6dbd237ce67ad
callum-jpg <callumtc.jpg@gmail.com>
2025-04-09 11:18:41 +0100

    better n_worker definition;

--- src/feature_blocks/backend/_backend.py
+++ src/feature_blocks/backend/_backend.py
@@ -8,42 +8,29 @@ from dask.delayed import Delayed
 
 log = logging.getLogger(__name__)
 
-def run_dask_backend(functions: list[Callable]):
-    # n_cpus: int = os.cpu_count()
-    n_cpus = 6
-    
-    if len(functions) < n_cpus:
-        n_workers = len(functions)
-    else:
-        n_workers = n_cpus
+def get_n_workers():
+    # Check if it's a SLURM job
+    slurm_cpus = os.getenv("SLURM_CPUS_ON_NODE")
 
-    log.info(f"Using Dask backend with {n_workers} n_workers.")
+    if slurm_cpus is not None:
+        return int(slurm_cpus)
+    else:
+        return os.cpu_count()
+
+
+def run_dask_backend(functions: list[Callable]):
+    n_workers = get_n_workers()
+    
+    if len(functions) < n_workers:
+        n_workers = len(functions)
+
+    log.info(f"Using Dask backend with {n_workers} workers.")
 
     with Client(n_workers=n_workers) as client:
-        ram_per_worker = client.cluster.workers[0].memory_manager.memory_limit
-
-        if ram_per_worker < 4 * 1024**3:
-            log.warning(
-                f"Each worker has less than 4GB of RAM ({ram_per_worker / 1024**3:.2f}GB). Consider increasing available memory. "
-            )
-
         if not all([isinstance(fn, Delayed) for fn in functions]):
             functions = [dask.delayed(fn)() for fn in functions]
 
-        # print("Are they all delayed?", {type(f) for f in functions})
-        # futures = dask.persist(*functions)
-        # print("Are futures all delayed?", {type(f) for f in futures})
-        # progress(futures)
-        # return client.gather(client.compute(list(futures)))
-
         futures = client.compute(functions)
-        print("Are futures all delayed?", {type(f) for f in futures})
         progress(futures)
         results = client.gather(futures)
-        print("Is results all delayed?", {type(f) for f in results})
         return results
-
-        # futures = [client.submit(fn) for fn in functions]
-        # progress(futures)
-        # results = client.gather(futures)
-        # return results
