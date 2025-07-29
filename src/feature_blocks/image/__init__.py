d2eb1081ab8a9b4d3a638ab6aef6a1bec14c5e40
callum-jpg <callumtc.jpg@gmail.com>
2025-07-29 13:34:24 +0100

    import

--- src/feature_blocks/image/__init__.py
+++ src/feature_blocks/image/__init__.py
@@ -1,2 +1,2 @@
-from ._image import load_tiff_scale, normalise_rgb, standardise_image
+from ._image import load_tiff_scale, normalise_rgb, standardise_image, zarr_exists
 from ._mask import tissue_detection
